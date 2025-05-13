f9217feb7d0fa0b4e0fc81a87dc496670c988cf4
kanaimandal2002 <148377285+kanaimandal2002@users.noreply.github.com>
2025-05-14 03:36:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+app.get('/users', (req, res) => {
+  res.json([{ id: 1, name: "Alice" }]);
+});
+
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
 
