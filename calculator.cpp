e71334f65ea6b00b822179aa942de4dd41cd76c7
kanaimandal2002 <148377285+kanaimandal2002@users.noreply.github.com>
2025-05-19 21:26:02 +0530

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -2,9 +2,18 @@
 using namespace std;
 
 int main() {
-  printf ("hello!");
+    char op;
+    double a, b;
+    cout << "Enter expression (e.g. 2 + 3): ";
+    cin >> a >> op >> b;
+
+    switch(op) {
+        case '+': cout << a + b; break;
+        case '-': cout << a - b; break;
+        case '*': cout << a * b; break;
+        case '/': cout << a / b; break;
+        default: cout << "Invalid operator";
     }
 
     return 0;
 }
-
